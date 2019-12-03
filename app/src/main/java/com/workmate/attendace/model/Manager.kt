082333d17a4a6b250f852ebc52107ab6feb06f0e
James Erickson Licong <james.licong@gmail.com>
2019-12-04 00:50:11 +0800

    Add Manager data class

---
+++ app/src/main/java/com/workmate/attendace/model/Manager.kt
@@ -0,0 +1,9 @@
+package com.workmate.attendace.model
+
+data class Manager(
+    private val id: String,
+    private val name: String,
+    private val email: String,
+    private val phone: String,
+    private val role: String
+)
