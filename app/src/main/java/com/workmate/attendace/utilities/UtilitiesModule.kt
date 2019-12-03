a032331f4c44e5d77af21b423d446d4b69008383
James Erickson Licong <james.licong@gmail.com>
2019-12-03 21:11:59 +0800

    Update UtilitiesModuel: Add reference of RetrofitModule

--- app/src/main/java/com/workmate/attendace/utilities/UtilitiesModule.kt
+++ app/src/main/java/com/workmate/attendace/utilities/UtilitiesModule.kt
@@ -1,10 +1,12 @@
 package com.workmate.attendace.utilities
 
+import com.workmate.attendace.utilities.retrofit.RetrofitModule
 import com.workmate.attendace.utilities.rx.RxModule
 import dagger.Module
 
 @Module(
     includes = [
+        RetrofitModule::class,
         RxModule::class
     ]
 )
