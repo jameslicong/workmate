a9921520c6a5f78ff29bfcf114617f00e885a79d
James Erickson Licong <james.licong@gmail.com>
2019-12-03 22:20:50 +0800

    Update HelpsterTechRestApi#login: Change return type from Response<String> to ApiKey data class

--- app/src/main/java/com/workmate/attendace/utilities/retrofit/HelpsterTechRestApi.kt
+++ app/src/main/java/com/workmate/attendace/utilities/retrofit/HelpsterTechRestApi.kt
@@ -1,15 +1,15 @@
 package com.workmate.attendace.utilities.retrofit
 
+import com.workmate.attendace.model.ApiKey
 import com.workmate.attendace.model.User
 import io.reactivex.Single
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface HelpsterTechRestApi {
 
-    @POST("auth/login")
+    @POST("auth/login/")
     fun login(
         @Body user: User
-    ): Single<Response<String>>
+    ): Single<ApiKey>
 }
