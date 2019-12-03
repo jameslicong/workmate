b12f225f94c89333872288584b3bdfb0800652cb
James Erickson Licong <james.licong@gmail.com>
2019-12-03 22:53:01 +0800

    Add SharedPreferenceCache

---
+++ app/src/main/java/com/workmate/attendace/utilities/preference/Cache.kt
@@ -0,0 +1,13 @@
+package com.workmate.attendace.utilities.preference
+
+interface Cache {
+
+    fun getLong(key: String): Long?
+
+    fun getString(key: String): String?
+
+    fun setLong(key: String, value: Long)
+
+    fun setString(key: String, value: String)
+
+}
