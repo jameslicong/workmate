4fdd2139510a9c6bebd4e01178ccda4c8bb7dceb
James Erickson Licong <james.licong@gmail.com>
2019-12-03 23:26:06 +0800

    Add Constants class

---
+++ app/src/main/java/com/workmate/attendace/utilities/Constants.kt
@@ -0,0 +1,10 @@
+package com.workmate.attendace.utilities
+
+class Constants {
+
+    object SharedPrefKeys {
+        val API_KEY = "apiKey"
+        val CLOCK_IN = "clock_in"
+        val CLOCK_OUT = "clock_out"
+    }
+}
