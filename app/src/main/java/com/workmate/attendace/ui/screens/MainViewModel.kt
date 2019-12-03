58c9169e18b2bee277a6e99747ec04ee78840e78
James Erickson Licong <james.licong@gmail.com>
2019-12-04 00:52:17 +0800

    WIP MainViewModel

--- app/src/main/java/com/workmate/attendace/ui/screens/MainViewModel.kt
+++ app/src/main/java/com/workmate/attendace/ui/screens/MainViewModel.kt
@@ -1,7 +1,54 @@
 package com.workmate.attendace.ui.screens
 
-import androidx.lifecycle.ViewModel
+import com.workmate.attendace.model.ApiKey
+import com.workmate.attendace.usecase.LoginAuthenticator
+import com.workmate.attendace.usecase.remote.JobInfoRemoteLoader
+import com.workmate.attendace.utilities.framework.BaseViewModel
+import com.workmate.attendace.utilities.rx.RxSchedulerUtils
+import timber.log.Timber
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+class MainViewModel
+
+    @Inject
+    internal constructor(
+        private val loginAuthenticator: LoginAuthenticator,
+        private val jobInfoRemoteLoader: JobInfoRemoteLoader,
+        private val scheduler: RxSchedulerUtils
+    ) : BaseViewModel() {
     // TODO: Implement the ViewModel
+
+    fun autoLogin() {
+
+        addDisposable(
+            loginAuthenticator.login()
+                .compose(scheduler.forCompletable())
+                .subscribe(
+                    {
+                        Timber.d("LOGIN SUCCESSFUL")
+                        loadJobInformation()
+                    },
+                    {
+                        it.printStackTrace()
+                        Timber.e("LOGIN FAILED ${it.message}")
+                    }
+                )
+        )
+    }
+
+    private fun loadJobInformation() {
+        addDisposable(
+            jobInfoRemoteLoader.load(ApiKey("e945ae028e2355e123cfdf1b4fbb81ad4e5b2ebc"))
+                .compose(scheduler.forSingle())
+                .subscribe(
+                    {
+                        Timber.d("JOB INFO $it")
+                    },
+                    {
+                        it.printStackTrace()
+                        Timber.e("FAILED TO LOAD JOB INFO ${it.message}")
+                    }
+                )
+        )
+    }
 }
