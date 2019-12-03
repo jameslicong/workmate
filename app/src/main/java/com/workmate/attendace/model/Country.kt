97d3ebecb05b01bb366167bacffca3da33d2f0c2
James Erickson Licong <james.licong@gmail.com>
2019-12-04 00:49:43 +0800

    Add Country data class

---
+++ app/src/main/java/com/workmate/attendace/model/Country.kt
@@ -0,0 +1,9 @@
+package com.workmate.attendace.model
+
+data class Country(
+    private val id: String,
+    private val name: String,
+    private val code: String,
+    private val currency_code: String,
+    private val dial_code: String
+)
