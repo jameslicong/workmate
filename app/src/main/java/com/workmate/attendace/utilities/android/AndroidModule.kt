e0be375bf8429d97a5a0afbb5c69292accbb318d
James Erickson Licong <james.licong@gmail.com>
2019-12-03 20:27:10 +0800

    Add AndroidModule

---
+++ app/src/main/java/com/workmate/attendace/utilities/android/AndroidModule.kt
@@ -0,0 +1,16 @@
+package com.workmate.attendace.utilities.android
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AndroidModule {
+
+    @Singleton
+    @Provides
+    fun provideContext(application: WorkmateAttendanceApplication): Context {
+        return application.applicationContext
+    }
+}
