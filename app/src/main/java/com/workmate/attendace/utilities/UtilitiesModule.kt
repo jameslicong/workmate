da4a46f7225f7019cc99a197e4a4eb152eafbdce
James Erickson Licong <james.licong@gmail.com>
2019-12-03 21:35:31 +0800

    Update UtilitiesModuel: include the FrameworkModule

--- app/src/main/java/com/workmate/attendace/utilities/UtilitiesModule.kt
+++ app/src/main/java/com/workmate/attendace/utilities/UtilitiesModule.kt
@@ -1,11 +1,13 @@
 package com.workmate.attendace.utilities
 
+import com.workmate.attendace.utilities.framework.FrameworkModule
 import com.workmate.attendace.utilities.retrofit.RetrofitModule
 import com.workmate.attendace.utilities.rx.RxModule
 import dagger.Module
 
 @Module(
     includes = [
+        FrameworkModule::class,
         RetrofitModule::class,
         RxModule::class
     ]
