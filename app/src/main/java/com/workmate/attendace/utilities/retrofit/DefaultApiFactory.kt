0dd36ad98a6a2d5714576b0ee10c93af4766f97c
James Erickson Licong <james.licong@gmail.com>
2019-12-04 21:11:23 +0800

    Update DefaultApiFactory: Add Logging Interceptor

--- app/src/main/java/com/workmate/attendace/utilities/retrofit/DefaultApiFactory.kt
+++ app/src/main/java/com/workmate/attendace/utilities/retrofit/DefaultApiFactory.kt
@@ -6,8 +6,10 @@ import com.workmate.attendace.utilities.Constants.SharedPrefKeys
 import com.workmate.attendace.utilities.preference.Cache
 import io.reactivex.Single
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class DefaultApiFactory
@@ -40,20 +42,39 @@ class DefaultApiFactory
             })
     }
 
+    private fun addLoggingInterceptor(
+        builder: OkHttpClient.Builder): Single<OkHttpClient.Builder> {
+        return Single.fromCallable { builder }
+            .map {
+                val loggingInterceptor = HttpLoggingInterceptor()
+                loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                it.addInterceptor(loggingInterceptor)
+                it.readTimeout(60, TimeUnit.SECONDS)
+                it
+            }
+    }
+
     private fun client(): Single<OkHttpClient> {
         return Single.fromCallable { OkHttpClient.Builder() }
+            .flatMap { addLoggingInterceptor(it) }
             .map { it.build() }
 
     }
 
+
     private fun clientWitApiKey(): Single<OkHttpClient> {
         return Single.fromCallable { OkHttpClient.Builder() }
+            .flatMap { addLoggingInterceptor(it) }
             .flatMap { addInterceptorForApiKey(it) }
             .map { it.build() }
     }
 
     private fun intoRetrofit(client: OkHttpClient): Single<Retrofit> {
         return Single.fromCallable {
+
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
             Retrofit.Builder()
                 .baseUrl(BuildConfig.SERVER_URL)
                 .client(client)
