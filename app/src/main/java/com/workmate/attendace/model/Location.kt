e9a3e0f875793b71fbbeb16020e062438039a032
James Licong <james.licong@gmail.com>
2019-12-06 09:31:12 +0800

    Update Location data class: Replace Country field to Address

--- app/src/main/java/com/workmate/attendace/model/Location.kt
+++ app/src/main/java/com/workmate/attendace/model/Location.kt
@@ -3,7 +3,7 @@ package com.workmate.attendace.model
 data class Location(
     val id: String,
     val name: String,
-    val country: Country,
+    val address: Address,
     val is_primary: Boolean,
     val contact_name: String,
     val contact_phone: String?
