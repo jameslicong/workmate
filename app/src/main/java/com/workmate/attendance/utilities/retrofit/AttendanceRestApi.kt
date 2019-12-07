9b3940f0ef0bc36a2a07c40d4286e2e109b091b0
James Erickson Licong <james.licong@gmail.com>
2019-12-08 00:41:04 +0800

    Update AttendanceRestApi#clockIn: Change response data model from Attendance to Timesheet

--- app/src/main/java/com/workmate/attendance/utilities/retrofit/AttendanceRestApi.kt
+++ app/src/main/java/com/workmate/attendance/utilities/retrofit/AttendanceRestApi.kt
@@ -2,6 +2,7 @@ package com.workmate.attendance.utilities.retrofit
 
 import com.workmate.attendance.model.Attendance
 import com.workmate.attendance.model.AttendanceLocation
+import com.workmate.attendance.model.Timesheet
 import io.reactivex.Single
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,8 +12,7 @@ interface AttendanceRestApi {
     @POST("staff-requests/{job-id}/clock-in/")
     fun clockIn(
         @Path("job-id") jobId: String,
-        @Body attendanceLocation: AttendanceLocation
-    ): Single<Attendance>
+        @Body attendanceLocation: AttendanceLocation): Single<Timesheet>
 
     @POST("staff-requests/{job-id}/clock-out/")
     fun clockOut(
