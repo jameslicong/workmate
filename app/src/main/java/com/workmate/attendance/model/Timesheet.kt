eae5215fbda45e3c93b8049a6218bf57a3eb3e94
James Erickson Licong <james.licong@gmail.com>
2019-12-08 00:39:07 +0800

    Update Timesheet data class: make all fields of clock out nullable

--- app/src/main/java/com/workmate/attendance/model/Timesheet.kt
+++ app/src/main/java/com/workmate/attendance/model/Timesheet.kt
@@ -3,10 +3,10 @@ package com.workmate.attendance.model
 data class Timesheet (
     val id: String,
     val clock_in_time: String,
-    val clock_out_time: String,
+    val clock_out_time: String?,
     val clock_in_latitude: String,
     val clock_in_longitude: String,
-    val clock_out_latitude: String,
-    val clock_out_longitude: String,
+    val clock_out_latitude: String?,
+    val clock_out_longitude: String?,
     val schedule: String?
 )
